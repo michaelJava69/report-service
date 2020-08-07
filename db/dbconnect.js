3956b1928d8a240486e4f7a41bf5d05a368cf63d
michaelJava69 <michael.professional@inbox.com>
2020-08-07 19:00:18 +0100

    Update dbconnect.js

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,12 +1,40 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const dbConnectString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/reportstore'
+const dbConnectString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongo:27017/reportstore'
+
+// to work on local machine n ot using docker-compose
+//const dbConnectString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/reportstore'
+
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+wait(9000);
+
+
+mongoose
+  .connect(dbConnectString, { useNewUrlParser: true })
+  .catch(e => {
+      console.error('Connection Error: after waited 9 seconds will try again after 9 secs  ', e.message)
+  });
+
+
+wait(9000);
+
+
 mongoose
   .connect(dbConnectString, { useNewUrlParser: true })
   .catch(e => {
       console.error('Connection Error: ', e.message)
   });
+
+
 
 const db = mongoose.connection;
 
